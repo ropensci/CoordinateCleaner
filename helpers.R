d416d4e591b59c653c955dec2c3603be92821f8b
azizka <alexander.zizka@bioenv.gu.se>
2016-11-01 17:24:42 +0100

    new backround data
    Former-commit-id: e316662f440ed154bceddeb4136bb433902f92a7 [formerly e316662f440ed154bceddeb4136bb433902f92a7 [formerly dc40260157bbc76388fc7ef8c601b49419409349]]
    Former-commit-id: a1c3a229dc1bb18300fe24200ab47a6b3708e3e4
    Former-commit-id: 9ce81f874df7810ab856e5f559e24b1b468279f3

---
+++ helpers.R
@@ -0,0 +1,249 @@
+.CapitalCoordinates <- function(x, testdist = 0.1, buffer = 1, referencedat = NULL) {
+  dat <- sp::SpatialPoints(x)
+  if (is.null(referencedat)) {
+    referencedat <- speciesgeocodeR::capitals
+  }
+  
+  limits <- raster::extent(dat) + buffer
+  
+  # subset of testdatset according to limits
+  referencedat <- raster::crop(SpatialPoints(referencedat[, c("longitude", "latitude")]), limits)
+  if(is.null(referencedat)){ # incase no capitals are found in the study area
+    out <- rep(TRUE, nrow(x))
+  }else{
+    referencedat <- rgeos::gBuffer(referencedat, width = testdist, byid = T)
+    out <- is.na(sp::over(x = dat, y = referencedat))
+  }
+  return(out)
+}
+
+.CentroidCoordinates <- function(x, testdist = 0.1, buffer = 1, testtype = c("both", "country", "provinces"), referencedat = NULL) {
+  dat <- sp::SpatialPoints(x)
+  if (is.null(referencedat)) {
+    referencedat <- speciesgeocodeR::centroids
+    
+    if (testtype[1] == "country") {
+      referencedat <- referencedat[referencedat$type == "country", ]
+    }
+    if (testtype[1] == "province") {
+      referencedat <- referencedat[referencedat$type == "province", ]
+    }
+  }
+  
+  limits <- raster::extent(dat) + buffer
+  
+  # subset of testdatset according to limits
+  referencedat <- raster::crop(SpatialPoints(referencedat[, c("longitude", "latitude")]), limits)
+  if(is.null(referencedat)){ # incase no capitals are found in the study area
+    out <- rep(TRUE, nrow(x))
+  }else{
+    referencedat <- rgeos::gBuffer(referencedat, width = testdist, byid = T)
+    out <- is.na(sp::over(x = dat, y = referencedat))
+  }
+  return(out)
+}
+
+.CountryCheck <- function(x, countries, poly = NULL) {
+  pts <- SpatialPoints(x)
+  
+  if (is.null(poly)) {
+    poly <- speciesgeocodeR::countryborders
+  }
+  
+  testpolys <- crop(poly, extent(pts))
+  
+  country <- sp::over(x = pts, y = testpolys)[, "ISO2"]
+  out <- as.character(country) == as.character(countries)
+  
+  return(out)
+}
+
+.OutlierCoordinates <- function(x, species, mltpl, tdi, method = "Haversine") {
+  
+  splist <- split(x, f = as.character(species))
+  
+  test <- lapply(splist, "duplicated")
+  test <- lapply(test, "!")
+  test <- as.vector(unlist(lapply(test, "sum")))
+  splist <- splist[test > 2]
+  
+  flags <- lapply(splist, function(k, td = tdi, mu = mltpl) {
+    
+    test <- nrow(k[!duplicated(k), ])
+    dist <- geosphere::distm(k, fun = distHaversine)
+    dist[dist == 0] <- NA
+    
+    if (!is.null(mu) & !is.null(td)) {
+      stop("set outliers.td OR outliers.mtp, the other one to NULL")
+    }
+    
+    if (!is.null(mu)) {
+      mins <- apply(dist, 1, min, na.rm = T)
+      quo <- quantile(mins, c(0.99), na.rm = T)
+      out <- which(mins > (quo + mean(mins, na.rm = T) * mu))
+    }
+    
+    if (!is.null(td)) {
+      mins <- apply(dist, 1, min, na.rm = T)
+      out <- which(mins > td * 1000)
+    }
+    
+    if (length(out) == 0) {
+      ret <- NA
+    }
+    if (length(out) > 0) {
+      ret <- rownames(k)[out]
+    }
+    return(ret)
+  })
+  
+  
+  flags <- as.numeric(as.vector(unlist(flags)))
+  flags <- flags[!is.na(flags)]
+  
+  out <- rep(TRUE, nrow(x))
+  out[flags] <- FALSE
+  
+  return(out)
+}
+
+.UrbanCoordinates <- function(x, poly = NULL) {
+  pts <- SpatialPoints(x)
+  limits <- extent(pts) + 1
+  
+  if (is.null(poly)) {
+    poly <- speciesgeocodeR::urbanareas
+  }
+  
+  poly <- crop(poly, limits)
+  
+  urban <- over(x = pts, y = poly)[, 1]
+  out <- is.na(urban)
+  
+  return(out)
+}
+
+.GBIF <- function(x) {
+  pts <- sp::SpatialPoints(x)
+  poly <- rgeos::gBuffer(SpatialPoints(cbind(12.58, 55.67)), width = 0.5)
+  warning("running GBIF test, flagging records around Copenhagen")
+  
+  out <- sp::over(x = pts, y = poly)
+  out <- is.na(out)
+  
+  return(out)
+}
+
+.ValidCoordinates <- function(x) {
+  out <- list(is.na(x$decimallongitude), is.na(x$decimallatitude), suppressWarnings(is.na(as.numeric(as.character(x$decimallongitude)))), suppressWarnings(is.na(as.numeric(as.character(x$decimallatitude)))), 
+              suppressWarnings(as.numeric(as.character(x$decimallongitude))) < -180, suppressWarnings(as.numeric(as.character(x$decimallongitude))) > 180, suppressWarnings(as.numeric(as.character(x$decimallatitude))) < 
+                -90, suppressWarnings(as.numeric(as.character(x$decimallatitude))) > 90)
+  
+  out <- !Reduce("|", out)
+  return(out)
+}
+
+.WaterCoordinates <- function(x, poly = NULL) {
+  pts <- SpatialPoints(x)
+  
+  if (is.null(poly)) {
+    testpolys <- speciesgeocodeR::landmass
+    testpolys <- crop(testpolys, extent(pts) + 1)
+  } else {
+    testpolys <- poly
+  }
+  land <- over(x = pts, y = testpolys)[, 1]
+  out <- !is.na(land)
+  
+  return(out)
+}
+
+.ZeroCoordinates <- function(x, pointlim = 0.5) {
+  pts <- SpatialPoints(x)
+  out <- rep(T, nrow(x))
+  
+  # plain zero in coordinates
+  out[which(x$decimallongitude == 0 | x$decimallatitude == 0)] <- FALSE
+  
+  # radius around point 0/0
+  test <- rgeos::gBuffer(sp::SpatialPoints(cbind(0, 0)), width = pointlim)
+  out[which(!is.na(over(y = test, x = pts)))] <- FALSE
+  
+  # lat == long
+  out[which(x$decimallongitude == x$decimallatitude)] <- FALSE
+  
+  return(out)
+} 
+
+plotter <- function(x, bgmap = NULL, clean = T, details = T, ...){
+  
+  #prepare background
+  e <- raster::extent(SpatialPoints(x[, 1:2])) + 1
+  
+  if (is.null(bgmap)) {
+    load("landmass.rda")
+    bgmap <- landmass
+    bgmap <- raster::crop(bgmap, e)
+  }
+  
+  bgmap <- ggplot2::fortify(bgmap)
+  
+  #plot background
+  plo <- ggplot2::ggplot()+
+    geom_polygon(data = bgmap, aes(x = long, y = lat, group = group), fill = "grey60")+
+    coord_fixed()+
+    theme_bw()
+  
+  #prepare occurence points
+  inv <- x
+  inv[,-c(1:2)] <- !inv[,-c(1:2)]
+  occs <- names(inv)[unlist(lapply(apply(inv == 1, 1, "which"), "[", 1))]
+  
+  if(length(occs) == 0){
+    occs <- rep("AAAclean", nrow(x))
+  }else{
+    occs[is.na(occs)] <- "AAAclean"
+  }
+  
+  occs <- cbind(x[,c("decimallongitude", "decimallatitude", "summary")], flag = occs)
+  if(!"AAAclean" %in% occs$flag){
+    clean <- FALSE
+    warnings("All records were flagged, setting clean to FALSE")
+  }
+  
+  #add points to background
+  if(!clean & !details){
+    pts <- occs[!occs$summary,]
+    plo <- plo+
+      ggplot2::geom_point(data = pts, aes(x = decimallongitude, y = decimallatitude), colour = "#F8766D") 
+  }
+  
+  if(clean & !details){
+    pts <- occs
+    plo <- plo+
+      ggplot2::geom_point(data = pts, aes(x = decimallongitude, y = decimallatitude, colour = summary))+
+      ggplot2::scale_colour_manual(values = c("#F8766D", "#00BFC4"), labels = c("Flagged", "Clean"))+
+      ggplot2::theme(legend.title=element_blank())
+  }
+  
+  if(!clean & details){
+    pts <- occs[!occs$summary,]
+    plo <- plo+
+      ggplot2::geom_point(data = pts, aes(x = decimallongitude, y = decimallatitude, shape = flag), colour = "#F8766D")+
+      ggplot2::theme(legend.title=element_blank())
+  }
+  
+  if(clean & details){
+    pts <- occs
+    plo <- plo+
+      ggplot2::geom_point(data = pts, aes(x = decimallongitude, y = decimallatitude, shape = flag, colour = flag))+
+      ggplot2::scale_colour_manual(values = c("#00BFC4", rep("#F8766D", length(unique(pts$flag)))),
+                                   breaks = as.character(unique(pts$flag)),
+                                   labels = c("clean", as.character(unique(pts$flag))[-1]))+
+      ggplot2::scale_shape_manual(values = c(16, seq(15, 15+(length(unique(pts$flag))-1))),
+                                  breaks = as.character(unique(pts$flag)),
+                                  labels = c("clean", as.character(unique(pts$flag))[-1]))+
+      ggplot2::theme(legend.title=element_blank())
+  }
+  plo
+} 
--- server.R
+++ server.R
@@ -1,23 +1,158 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(sp)
+library(raster)
+library(geosphere)
+library(rgeos)
+
+source("helpers.R")
+
+#hardcode options
+buffer <- 1
+capitals.rad <- 0.1
+centroids.rad <- 0.1
+centroid.details <- "both"
+outliers.mtp <- 25
+outliers.td <- NULL
+zeros.rad <- 0.5
+
+#add plotting
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  #set maximum file size
+  options(shiny.maxRequestSize=35*1024^2) # change maximum upload size
+  
+  #load the input data
+  dataInput <- reactive({
+    inFile <- input$file1
+    
+    read.csv(inFile$datapath, sep = "\t")
   })
+  
+  #Define input values
+  
+  x <- reactive({data.frame(dataInput()[,c("decimallongitude", "decimallatitude")])})
+  species <- reactive({dataInput()[,"species"]})
+  countries <- reactive({dataInput()[,"countrycode"]})
+  
+  #capital coordinates
+ cap <- reactive({if(input$cap){
+    load("capitals.rda")
+    .CapitalCoordinates(x(), testdist = capitals.rad, buffer = buffer, 
+                               referencedat = capitals)
+  } else {
+    rep(NA, nrow(x()))
+  }})
+ 
+  #Country check
+ con <- reactive({if(input$con){
+   load("countryborders.rda")
+   .CountryCheck(x(), countries = countries(), poly = countryborders)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #Country centroids
+ cen <- reactive({if(input$cen){
+   load("centroids.rda")
+   .CentroidCoordinates(x(), testdist = centroids.rad, buffer = buffer, 
+                               testtype = centroids.detail, referencedat = centroids)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #Duplicates
+ dpl <- reactive({if(input$dpl){
+   if (is.null(species())) {
+     dpl.test <- x()
+   } else {
+     dpl.test <- data.frame(x(), species())
+   }
+   !duplicated(dpl.test)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #GBIF
+ gbf <- reactive({if(input$gbf){
+   .GBIF(x())
+ } else {
+    rep(NA, nrow(x()))
+ }})
+ 
+ #Outliers
+ otl <- reactive({if(input$otl){
+   .OutlierCoordinates(x(), species = species(), mltpl = outliers.mtp, tdi = outliers.td)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #Seas
+ sea <- reactive({if(input$sea){
+   load("landmass.rda")
+   .WaterCoordinates(x())
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #Urban
+ urb <- reactive({if(input$urb){
+   load("urbanareas.rda")
+   .UrbanCoordinates(x(), poly = urbanareas)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ #Validity
+ val <- reactive( 
+   if(input$val){
+     .ValidCoordinates(x())
+   } else {
+     rep(NA, nrow(x()))
+   }
+ )
+ 
+ #Zeros
+ zer <- reactive({if(input$zer){
+   .ZeroCoordinates(x(), pointlim = zeros.rad)
+ } else {
+   rep(NA, nrow(x()))
+ }})
+ 
+ out <- reactive({
+   out <- list(val(), zer(), cap(), cen(), sea(), urb(), con(), otl(), gbf(), dpl())
+   out <- Filter(function(k) !all(is.na(k)), out)
+   out <- Reduce("&", out)
+   
+   out <- data.frame(x(), capitals = cap(), countrycheck = con(), centroids = cen(),
+                     duplicates = dpl(),  
+                     gbif = gbf(), outliers = otl(),  sea = sea(), urban = urb(), 
+                     validity = val(), zeros = zer(), summary = out)
+   Filter(function(k) !all(is.na(k)), out)
+ })
+  #switch to data tble or show summary instead
+  output$table <- renderTable({
+    if(is.null(input$file1))
+      return()
+    head(data.frame(out()))
+  })
+  
+  output$distPlot <- renderPlot({
+    if(is.null(input$file1))
+      return()
+    load("landmass.rda")
+    plotter(out(), clean = T, detail = T)
+  })
+  
+  #fix this
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("flagged_coordinates", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(out(), file)
+    }
+  )
+
 
 })
