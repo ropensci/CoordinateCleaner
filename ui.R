cbfe484d27b6a975b603d6b14fd9fba1a9fb5c73
azizka <alexander.zizka@bioenv.gu.se>
2016-11-02 13:09:15 +0100

    version 0.9
    Former-commit-id: 04e67ab3c4123e6886c6785032824b57d9fde1e2 [formerly 20ff5f04208fc4903fe052634399a319441cbfde [formerly 93f0a86428ca1ec99d3e74177af5db47860ad3a2] [formerly 75067a5080018518979aa33909be5ed531f15dfe] [formerly 04e67ab3c4123e6886c6785032824b57d9fde1e2 [formerly 04e67ab3c4123e6886c6785032824b57d9fde1e2 [formerly d0156eee1a0da809a5b3a3e5b525e3d0ed18c9a4]]]]
    Former-commit-id: 20ff5f04208fc4903fe052634399a319441cbfde [formerly 93f0a86428ca1ec99d3e74177af5db47860ad3a2] [formerly 75067a5080018518979aa33909be5ed531f15dfe]
    Former-commit-id: 20ff5f04208fc4903fe052634399a319441cbfde [formerly 93f0a86428ca1ec99d3e74177af5db47860ad3a2]
    Former-commit-id: 4b9bee9421c1a162f5641fcdbe17f54ee94f85dc
    Former-commit-id: b33d12d2f26fa4b9d8a2319cd33247ac8a4b603f

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(DT)
 library(shiny)
+library(shinyBS)
 
 shinyUI(fluidPage(
 
@@ -10,7 +11,8 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      fileInput("file1", label = h3("Input Data")),
+      fileInput("file1", label = h4("Input Data")),
+      h4("Tests"),
       checkboxInput("val", label = "Validity", value = T),
       checkboxInput("cap", label = "Capitals"),
       checkboxInput("cen", label = "Centroids"),
@@ -22,19 +24,47 @@ shinyUI(fluidPage(
       checkboxInput("sea", label = "Seas"),
       checkboxInput("zer", label = "Zeros"),
       
-    downloadButton('downloadData', label = 'Download Results')
+      numericInput("outl.dist", label = h4("Outlier threshold distance"), value = 1000),
+      
+      h4("Data Download"),
+    downloadButton('downloadData', label = 'Download Results'),
+    
+    bsPopover(id = "downloadData", title = "Download flags for each record", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "file1", title = "A tab delimited file containing columns named: decimallongitude, decimallatitude, and optionally species, and countrycode", 
+              placement = "right", trigger = "click"),
+    bsPopover(id = "outl.dist", title = "The maximum distance to the next point of the same species.", 
+              placement = "right", trigger = "click"),
+    bsPopover(id = "val", title = "Coordinates validity under lat/lon", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "cap", title = "0.1° radius around capitals", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "cen", title = "0.1° radius around country and province centroids", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "con", title = "Within the country specified by countrycode, needs a countrycode column in the input", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "dpl", title = "Identical species + identical coordinates", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "gbf", title = "0.5° radius around GBIF headquaters in Copenhagen", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "otl", title = "More than outlier threshold distance in km to the next record of the same species, needs a species column in the input", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "urb", title = "Within urbanized area", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "sea", title = "In the ocean", 
+              placement = "right", trigger = "hover"),
+    bsPopover(id = "zer", title = "Plain zero coordinates or 0.5° radius around 0/0, or lat = long", 
+              placement = "right", trigger = "hover")
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      fluidRow(
-       tableOutput("table")
-      ),
-      fluidRow(column(9,
-        plotOutput("distPlot")
-      ),
-      column(3,
-             tableOutput("summary")))
+      fluidRow(column(12,
+                      plotOutput("distPlot")
+               )),
+      
+      fluidRow(column(8, plotOutput("raster")),
+               column(4, tableOutput("log")))
     )
   )
 ))
