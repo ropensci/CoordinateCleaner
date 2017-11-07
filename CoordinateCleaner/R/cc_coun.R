6897f1237498d247d161eb7f4224d2aa977a9e85
azizka <alexander.zizka@bioenv.gu.se>
2017-11-07 09:01:59 +0100

    populating help files
    Former-commit-id: 03d6a4d44052d372fa8d46e67200d57375840342 [formerly 03d6a4d44052d372fa8d46e67200d57375840342 [formerly 8a7caa40a1304894984d60872e14977b3cdb8a64]]
    Former-commit-id: 2227049bb590e365a9f216bcde5ed6b5f93516b7
    Former-commit-id: 1b1b2c05f551bc0bf39de639b2f141f50bbb4b04

---
+++ CoordinateCleaner/R/cc_coun.R
@@ -0,0 +1,43 @@
+cc_coun <- function(x, lon = "decimallongitude", lat = "decimallatitude", iso3 = "countrycode",
+                          value = "clean", ref = NULL, verbose = TRUE) {
+  
+  #check function arguments for validity
+  match.arg(value, choices = c("clean", "flags"))
+  if(!"countrycode" %in% names(x)){stop("iso3 argument missing, please specify")}
+  
+  if(verbose){
+    cat("Testing country identity\n")
+  }
+
+  #set reference and check for dependency
+  if (is.null(ref)) {
+    if (!requireNamespace("rnaturalearth", quietly = TRUE)) {
+      stop("Please install the 'rnaturalearth' package for the country test or provide a custom reference via ref", 
+           call. = FALSE)
+    }
+    ref <- rnaturalearth::ne_countries(scale = "medium")
+    sp::proj4string(ref) <- ""
+  }else{
+    sp::proj4string(ref) <- ""
+    warning("assuming lat/lon for country.ref")
+  }
+  
+  #prepare data
+  dat <- sp::SpatialPoints(x[, c(lon, lat)])
+  sp::proj4string(ref) <- ""
+  ref <- raster::crop(ref, raster::extent(dat) + 1)
+  
+  #get country from coordinates and compare with provided country
+  country <- sp::over(x = dat, y = ref)[, "iso_a3"]
+  out <- as.character(country) == as.character(x[, iso3])
+  out[is.na(out)] <- TRUE #marine records are ignored / not tested
+  
+  #return output
+  if(verbose){
+    cat(sprintf("Flagged %s records. \n", sum(!out)))
+  }
+  
+  switch(value,
+         clean = return(x[out,]),
+         flags = return(out))
+}
---
+++ CoordinateCleaner/R/cc_outl.R
@@ -0,0 +1,81 @@
+cc_outl <- function(x, lon = "decimallongitude", lat = "decimallatitude", species = "species", 
+                   method = "quantile", mltpl = 3, tdi = 1000,
+                   value = "clean", verbose = T) {
+  
+  #check value argument
+  match.arg(value, choices = c("clean", "flags"))
+  match.arg(outl.method, choices = c("distance", "quantile", "mad"))
+  
+  if(verbose){
+    cat("Testing outliers\n")
+  }
+  
+  #split up into species
+  splist <- split(x, f = as.character(x[[species]]))
+  
+  #remove duplicate records and make sure that there are at least two records left
+  test <- lapply(splist, "duplicated")
+  test <- lapply(test, "!")
+  test <- as.vector(unlist(lapply(test, "sum")))
+  splist <- splist[test > 2]
+  
+  #loop over species and run outlier test
+  flags <- lapply(splist, function(k) {
+    test <- nrow(k[!duplicated(k), ])
+    
+    if(test >2){
+      #absolute distance test with mean interpoint distance
+      if (method == "distance") {
+        dist <- geosphere::distm(k[, c(lon, lat)], fun = geosphere::distHaversine)
+        dist[dist == 0] <- NA
+        
+        mins <- apply(dist, 1, min, na.rm = T)
+        out <- which(mins > tdi * 1000)
+      }
+      
+      #Quantile based test, with mean interpoint distances
+      if (method == "quantile") {
+        dist <- geosphere::distm(k[, c(lon, lat)], fun = geosphere::distHaversine)
+        dist[dist == 0] <- NA
+        
+        mins <- apply(dist, 1, mean, na.rm = T)
+        quo <- quantile(mins, c(0.25, 0.75), na.rm = T)
+        out <- which(mins < quo[1] - IQR(mins) * mltpl | mins > quo[2] + IQR(mins) * mltpl)
+      }
+      
+      #MAD (Median absolute deviation) based test, calculate the mean distance to all other points for each point, and then take the mad of this
+      if (method == "mad") {
+        dist <- geosphere::distm(k[, c(lon, lat)], fun = geosphere::distHaversine)
+        dist[dist == 0] <- NA
+        
+        mins <- apply(dist, 1, mean, na.rm = T)
+        quo <- median(mins)
+        tester <- mad(mins)
+        out <- which(mins < quo - tester * mltpl | mins > quo + tester * mltpl)
+      }
+    }
+    #create output object
+    if (length(out) == 0) {
+      ret <- NA
+    }
+    if (length(out) > 0) {
+      ret <- rownames(k)[out]
+    }
+    return(ret)
+  })
+  
+  flags <- as.numeric(as.vector(unlist(flags)))
+  out <- flags[!is.na(flags)]
+  
+  out <- rep(TRUE, nrow(x))
+  out[flags] <- FALSE
+  
+  if(verbose){
+    cat(sprintf("Flagged %s records. \n", sum(!out)))
+  }
+  
+  switch(value,
+         clean = return(x[out,]),
+         flags = return(out))
+}
+
