ebcd70f87fbf4d169067accfd3f2fcf91d7bc5ee
azizka <alexander.zizka@bioenv.gu.se>
2017-11-08 10:02:16 +0100

    Bug fix binary test dc_ddmm
    Former-commit-id: 76ed5bb6f98535f5ca9b636bf290c8ee71015fc2 [formerly ba0a7c6ad9581b2c377512b5f3a70db1f6da1e47 [formerly e98d1a6929eeaeac750c4bc2d94951298e2f0063] [formerly 8cc6d121da4d8e28e844667ddb971e17cbc07b6f] [formerly 76ed5bb6f98535f5ca9b636bf290c8ee71015fc2 [formerly 76ed5bb6f98535f5ca9b636bf290c8ee71015fc2 [formerly efb92df1e5e0c59fac362f590266e8f6b98f4955]]]]
    Former-commit-id: e39b769fc33e8b65edf05e7b333b7c5dcb0dffec
    Former-commit-id: e4e6b3329f9ee93f7e0ae3ef7de0719361d2657c
    Former-commit-id: c2150c1f2eab76a99973ad680421f3c5922f89dc
    Former-commit-id: 34523674ab064e1f396bc862d33c78eff7676b1c

---
+++ CoordinateCleaner.Rcheck/00_pkg_src/CoordinateCleaner/R/cc_inst.R
@@ -0,0 +1,40 @@
+cc_inst <- function(x, lon = "decimallongitude", lat = "decimallatitude",
+                    buffer = 0.001, ref = NULL,
+                    value = "clean", verbose = TRUE){
+  
+  #check value argument
+  match.arg(value, choices = c("clean", "flags"))
+  
+  if(verbose){
+    cat("Testing biodiversity insitutions\n")
+  }
+  
+  dat <- sp::SpatialPoints(x[, c(lon, lat)])
+  
+  #prepare reference dataset
+  if (is.null(ref)) {
+    ref <- CoordinateCleaner::institutions
+    ref <- ref[!is.na(ref$decimallongitude) & !is.na(ref$decimallatitude),]
+  }
+  limits <- raster::extent(dat) + buffer
+  
+  # subset of testdatset according to limits
+  ref <- raster::crop(sp::SpatialPoints(ref[, c("decimallongitude", "decimallatitude")]), limits)
+  
+  #test reference data after limiting and do test
+  if(is.null(ref)){ # in case no bdinstitutions
+    out <- rep(TRUE, nrow(x))
+  }else{
+    ref <- rgeos::gBuffer(ref, width = buffer, byid = T)
+    out <- is.na(sp::over(x = dat, y = ref))
+  }
+  
+  #create output based on value argument
+  if(verbose){
+    cat(sprintf("Flagged %s records\n", sum(!out)))
+  }
+  
+  switch(value,
+         clean = return(x[out,]),
+         flags = return(out))
+}
--- CoordinateCleaner/R/dc_ddmm.R
+++ CoordinateCleaner/R/dc_ddmm.R
@@ -1,6 +1,5 @@
 dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "dataset",
-                    pvalue = 0.025, diff = 0.2,
-                    value = "clean", verbose = TRUE){
+                    pvalue = 0.025, diff = 0.1, value = "clean", verbose = TRUE){
   
   #check value argument
   match.arg(value, choices = c("clean", "flags", "dataset"))
@@ -17,15 +16,15 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
   if (nrow(dat) == 0) {
     stop("no complete cases found")
   }
-  
+
   ## create test columns: decimal degrees long and lat
-  dat$lon.test <- abs(dat$decimallongitude) - floor(abs(dat$decimallongitude))
-  dat$lat.test <- abs(dat$decimallatitude) - floor(abs(dat$decimallatitude))
+  dat$lon.test <- abs(dat[[lon]]) - floor(abs(dat[[lon]]))
+  dat$lat.test <- abs(dat[[lat]]) - floor(abs(dat[[lat]]))
   
   # split into seperate datasets
   test <- split(dat, f = dat[[ds]])
   
-  # run Test 1 per dataset - ddmm to dd.dd conversion error at 0.6
+  # run ddmm to dd.dd conversion test error at 0.6
   out <- lapply(test, function(k) {
       ## create test datasets
       dat.unique <- k[!duplicated(k[, 1:2]), ]
@@ -34,7 +33,7 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
       r <- raster::raster(xmn = 0, xmx = 1, ymn = 0, ymx = 1)
       raster::res(r) <- 0.01
       
-      dat.t1 <- raster::rasterize(SpatialPoints(dat.unique[, c("lon.test", "lat.test")]), 
+      dat.t1 <- raster::rasterize(sp::SpatialPoints(dat.unique[, c("lon.test", "lat.test")]), 
                                   r, fun = "count")
       dat.t1 <- raster::as.matrix(dat.t1)
       dat.t1[is.na(dat.t1)] <- 0
@@ -42,7 +41,7 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
       # Binomial test, to see if more values are below 0.6 than expected
       P_smaller_than_06 <- 59 * 59/10000  # 0.3481
       
-      subt <- dat.t1[1:59, 1:59]  # subset tbl 
+      subt <- dat.t1[41:100, 1:59]  # subset tbl 
       p06 <- sum(subt >= 1)
       pAll <- sum(dat.t1 >= 1)
       
@@ -53,7 +52,7 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
       # PERCENTAGE OF difference from expected
       v2 <- (B$estimate - P_smaller_than_06)/P_smaller_than_06  
       
-      # These to thresholds could be changed
+      # These two thresholds could be changed
       if (v1 < pvalue & v2 > diff) {
         flag.t1 <- FALSE
       } else {
@@ -67,7 +66,7 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
     out.ds <- do.call("rbind.data.frame", out)
     names(out.ds) <- c("binomial.pvalue", "perc.difference", "pass")
     
-    flags <- x[[ds]] %in% rownames(out.ds[out.ds$pass == 1])
+    flags <- x[[ds]] %in% rownames(out.ds[out.ds$pass == 1,])
     
     
     # return output dependent on value argument
