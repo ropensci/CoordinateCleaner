5d2f5fa4ae2b3ad0610c721d71d40413dde48148
azizka <alexander.zizka@bioenv.gu.se>
2017-11-17 20:58:27 +0100

    minor bugfixes

--- CoordinateCleaner/R/cc_coun.R
+++ CoordinateCleaner/R/cc_coun.R
@@ -29,7 +29,7 @@ cc_coun <- function(x, lon = "decimallongitude", lat = "decimallatitude", iso3 =
   
   #get country from coordinates and compare with provided country
   country <- sp::over(x = dat, y = ref)[, "iso_a3"]
-  out <- as.character(country) == as.character(x[, iso3])
+  out <- as.character(country) == as.character(unlist(x[, iso3]))
   out[is.na(out)] <- TRUE #marine records are ignored / not tested
   
   #return output
