8fa59205a33e2ea5d8d2a8d4a23162fc3cc1f427
azizka <alexander.zizka@bioenv.gu.se>
2017-11-22 10:37:43 +0100

    incomplete data bugfix ddmm
    Former-commit-id: 0c777ff2f4d702cb206d5187b33039f6a02b9ef7 [formerly 194abc44d46f267285623e16b9ab47d0bf82a342] [formerly 90bd47102e08d37b451bdda13e790c478c9ec7b0] [formerly 8ea3902ecf65200fb44c59e752fcffae8cb084f0 [formerly 8ea3902ecf65200fb44c59e752fcffae8cb084f0 [formerly 5ddc9b99b52d46930cb7b47649526d27a8540978]]]
    Former-commit-id: 6c84e4c7ee8463bab1fe92564332c9f909a5773a
    Former-commit-id: e574a4154ac9cb0ed849d671c4303385ad2df26b
    Former-commit-id: 1af31331d87590df843f452a313c9ca3997c9703

--- CoordinateCleaner/R/dc_ddmm.R
+++ CoordinateCleaner/R/dc_ddmm.R
@@ -10,12 +10,12 @@ dc_ddmm <- function(x, lon = "decimallongitude", lat = "decimallatitude", ds = "
   }
   
   #prepare dataset for analyses
-  if (sum(!complete.cases(x)) > 0) {
+  if (sum(!complete.cases(x[, c(lon, lat, ds)])) > 0) {
     warning(sprintf("ignored %s cases with incomplete data", sum(!complete.cases(x))))
   }
   
   #get function data
-  dat <- x[complete.cases(x), ]
+  dat <- x[complete.cases(x[, c(lon, lat, ds)]), ]
   
   if (nrow(dat) == 0) {
     stop("no complete cases found")
