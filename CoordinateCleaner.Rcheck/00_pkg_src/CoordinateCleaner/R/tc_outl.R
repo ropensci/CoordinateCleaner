427a34b598aa86149f6cafa4bdc93cbdc46e6fa4
azizka <alexander.zizka@bioenv.gu.se>
2018-01-30 13:32:29 +0100

    v1.0-6 fix fossil functions
    Former-commit-id: 8570c996d62bbb16945cd96c324b5fee500403d4 [formerly df4e13a3db09204a0f5bf9705d33d525d6fd9ef2 [formerly bb1e7b9eed4bff6761c377efcf0b565442def843] [formerly 6214bfc95fdd0a14d5dc0f4a861daa85d7b2d7fb] [formerly 9bb2c5bfdbf0897b4a2cf0138b401d7b82f82678] [formerly fafd0370e972425877c11438d1b941ec40eee022 [formerly 8756e67e330bed1070604fcb0d37201868113ffb] [formerly 1dc8247745534d3c3e292b97a2b9ab37dc2334e8] [formerly 8570c996d62bbb16945cd96c324b5fee500403d4 [formerly 8570c996d62bbb16945cd96c324b5fee500403d4 [formerly be57a9225342a1b3ac7d1b5135d22b01e353707a]]]]]
    Former-commit-id: f21b99952d021b87aa10468328368bbe3ff1f2fc
    Former-commit-id: a7c638836403f9784a44bbc8bf5ba796ad740c3e [formerly 8813c3b1de4b36a67fcd2b18eb662da656719663] [formerly a564bc82b97b8111dacf9267c26d0252207f134d] [formerly 11a4a7d0a8894ee17a5b99317e2164df16184891]
    Former-commit-id: beef322af9b207c1ed0a34fa366a809c1354cffe
    Former-commit-id: cfd513c42c1fc554354a17180a5dc72f5f373313
    Former-commit-id: 79d0412dac5bc3dc5e100d0fca401c12f31bd46e

--- CoordinateCleaner.Rcheck/00_pkg_src/CoordinateCleaner/R/tc_outl.R
+++ CoordinateCleaner.Rcheck/00_pkg_src/CoordinateCleaner/R/tc_outl.R
@@ -1,11 +1,6 @@
-# questions: How to deal with the fact, that there are many records with the same coordinates and or time range/midpoint 
-#nrmalization correct? are mean values for the distances OK or any obvious problem? 
-#excluded duplications for dataset test? - No duplications of coordinates and timrange?
-
-
 tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "max_ma",
                     taxon = "accepted_name", method = "quantile", size.thresh = 7,
-                    mltpl = 3, replicates = 5, flag.thresh = 0.5,
+                    mltpl = 5, replicates = 5, flag.thresh = 0.5, uniq.loc = T,
                     value = "clean", verbose = T) {
   
   # check value argument
@@ -20,24 +15,31 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
       cat("Testing spatio-temporal outliers on taxon level\n")
     }
   }
+  
+  x$idf <- rownames(x)
 
   out <- replicate(replicates, expr = {
-  
-    
-    
-  # create testing data by simulating 100 points within the age range of each individal method fossil
+
+  # create testing data by simulating points within the age range of each individal method fossil
   x$samplepoint <- apply(X = x, 1, FUN = function(k){stats::runif(n = 1, 
-                                                         min = as.numeric(k[[max.age]], na.rm = T),
-                                                         max = as.numeric(k[[min.age]], na.rm = T))})
+                                                         min = as.numeric(k[[min.age]], na.rm = T),
+                                                         max = as.numeric(k[[max.age]], na.rm = T))})
   x$samplepoint <- round(x$samplepoint, 2)
+  
   
   if (taxon == "") {
     # select relevant columns
-    test <- x[, c(lon, lat, min.age, max.age, "samplepoint")]
+    test <- x[, c(lon, lat, min.age, max.age, "samplepoint", "idf")]
+    
+    #round coordinates to one decimal
+    test[, lon] <- round(test[, lon], 1)
+    test[, lat] <- round(test[, lat], 1)
     
     # remove duplicates
-    #test <- test[!duplicated(test[, c(lon, lat, min.age, max.age)]), ]
-    
+    if(uniq.loc){
+      test <- test[!duplicated(test[,c(lon, lat, min.age, max.age)]),]
+    }
+
     # calculate geographic distance
     if (nrow(test) < 10000) {
       dis.geo <- geosphere::distm(test[, c(lon, lat)], fun = geosphere::distHaversine)/1000
@@ -63,6 +65,7 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
       mins <- apply(dis, 1, mean, na.rm = T)
       quo <- quantile(mins, 0.75, na.rm = T)
       out <- which(mins > quo + IQR(mins, na.rm = T) * mltpl)
+      flags <- test[out, "idf"]
     }
     
     # MAD (Median absolute deviation) based test, calculate the mean distance to all other points for each point, and then take the mad of this
@@ -71,20 +74,26 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
       quo <- median(mins, na.rm = T)
       tester <- mad(mins, na.rm = T)
       out <- which(mins > quo + tester * mltpl)
+      flags <- test[out, "idf"]
     }
-    
-    flags <- rownames(test)[out]
-  
     } else {
     # select relevant columns
-    splist <- x[, c(lon, lat, min.age, max.age, "samplepoint", taxon)]
+    splist <- x[, c(lon, lat, min.age, max.age, "samplepoint", taxon, "idf")]
     
+    #round coordinates to one decimal
+    splist[, lon] <- round(splist[, lon], 1)
+    splist[, lat] <- round(splist[, lat], 1)
+    
+    #get unique occurrences
+    if(uniq.loc){
+      splist <- splist[!duplicated(splist[,c(taxon, lon, lat, min.age, max.age)]),]
+    }
     # split up into taxon
-    splist <- split(splist, f = as.character(x[[taxon]]))
-    
+    splist <- split(splist, f = as.character(splist[[taxon]]))
+
     # only test taxa with a minimum number of records
     test <- as.vector(unlist(lapply(splist, "nrow")))
-    splist <- splist[test > size.thresh]
+    splist <- splist[test >= size.thresh]
     
     # loop over taxon and run outlier test
     flags <- lapply(splist, function(k) {
@@ -114,6 +123,7 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
           mins <- apply(dis, 1, mean, na.rm = T)
           quo <- quantile(mins, 0.75, na.rm = T)
           out <- which(mins > quo + IQR(mins, na.rm = T) * mltpl)
+          out <- k[out, "idf"]
         }
         
         # MAD (Median absolute deviation) based test, calculate the mean distance to all other points for each point, and then take the mad of this
@@ -122,19 +132,18 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
           quo <- median(mins, na.rm = T)
           tester <- mad(mins, na.rm = T)
           out <- which(mins > quo + tester * mltpl)
+          out <- k[out, "idf"]
         }
         
         # create output object
         if (length(out) == 0) {
           ret <- NA
-        }
-        if (length(out) > 0) {
-          ret <- rownames(k)[out]
+        }else{
+          ret <- unlist(out)
         }
       }else{
         ret <- NA
       }
-      
      return(ret)
     })
   }
@@ -145,15 +154,35 @@ tc_outl <- function(x, lon = "lng", lat = "lat", min.age = "min_ma", max.age = "
   
   # create vector of logical flags
   out <- rep(TRUE, nrow(x))
-  out[rownames(x) %in% flags] <- FALSE
-  
+  out[flags] <- FALSE
 
   return(out)
-})
+  })
 
   frac <- apply(out, 1, "mean")
+  
   out <- frac >= flag.thresh
   
+  #also mark records that might not have been flagged due to the duplicate removal above
+  if(taxon == "" & any(!out)){
+    supp <- x[!out, c(lon, lat, min.age, max.age)]
+    supp$id <-  "tested"
+    supp <- merge(supp, x, by = c(lon, lat, min.age, max.age), all.x = T)
+    supp <- supp[, c("idf", "id")]
+    out[as.numeric(supp$idf)] <- FALSE
+  }else{
+    if(any(!out)){
+      supp <- x[!out, c(taxon, lon, lat, min.age, max.age)]
+      supp$id <-  "tested"
+      supp <- merge(supp, x, by = c(taxon, lon, lat, min.age, max.age), all.x = T)
+      supp <- supp[, c("idf", "id")]
+      out[as.numeric(supp$idf)] <- FALSE
+    }
+  }
+  
+  #remove identifier column
+  x <- x[,names(x) != "idf"]
+
   # report to screen
   if (verbose) {
       cat(sprintf("Flagged %s records. \n", sum(!out, na.rm = T)))
