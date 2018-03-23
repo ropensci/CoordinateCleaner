b1322538317cd26171a9daf4927b40a031f3a7b9
azizka <alexander.zizka@bioenv.gu.se>
2018-03-23 11:05:10 +0100

    dataset level test added to test suite
    Former-commit-id: dfad191db1b515c70f7310bea603baeebf99dd3e [formerly dfad191db1b515c70f7310bea603baeebf99dd3e [formerly e6d200fc158ce999ae98fc5372231753a00e4306]]
    Former-commit-id: 9ef0556129e479e4cc1b2d7218816ffaeda658ae
    Former-commit-id: 825017a9316e961728bc480562a904ef0a1f4d0c

--- tests/testthat/test_wrapper_functions.R
+++ tests/testthat/test_wrapper_functions.R
@@ -2,6 +2,8 @@ library(CoordinateCleaner)
 context("Coordinate cleaning")
 
 set.seed(1)
+
+# Coordinate level cleaning
 exmpl <- data.frame(species = sample(letters, size = 250, replace = TRUE),
                     decimallongitude = runif(250, min = 42, max = 51),
                     decimallatitude = runif(250, min = -26, max = -11))
@@ -9,4 +11,29 @@ exmpl <- data.frame(species = sample(letters, size = 250, replace = TRUE),
 test_that("cc_cap arguments work", {
   expect_equal(ncol(CleanCoordinates(x = exmpl)), 12)
   expect_equal(nrow(CleanCoordinates(x = exmpl)), 250)
+})
+
+
+#Dataset level cleaning
+#Create test dataset
+clean <- data.frame(dataset = rep("clean", 1000),
+                    decimallongitude = runif(min = -42, max = -40, n = 1000),
+                    decimallatitude = runif(min = -12, max = -10, n = 1000))
+
+bias.long <- c(round(runif(min = -42, max = -40, n = 500), 1),
+               round(runif(min = -42, max = -40, n = 300), 0),
+               runif(min = -42, max = -40, n = 200))
+bias.lat <- c(round(runif(min = -12, max = -10, n = 500), 1),
+              round(runif(min = -12, max = -10, n = 300), 0),
+              runif(min = -12, max = -10, n = 200))
+bias <- data.frame(dataset = rep("biased", 1000),
+                   decimallongitude = bias.long,
+                   decimallatitude = bias.lat)
+test <- rbind(clean, bias)
+
+
+test_that("dataset levele cleaning works", {
+  skip_on_cran()
+  expect_is(CleanCoordinatesDS(test), "data.frame")
+  expect_equal(sum(CleanCoordinatesDS(test)$summary), 1)
 })
