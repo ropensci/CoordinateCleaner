758f1548cc6c56ab860cc42de4c32dced0fcbf6a
azizka <alexander.zizka@bioenv.gu.se>
2018-04-08 21:45:20 +0200

    expand test suite
    Former-commit-id: 1cf26513b09dfbd440a0033224c9687210a80eb8 [formerly af8cba2a6a7309f947dbf6b3ba210aaa093421bc [formerly ce4f679e557e389b4ed8bc3e8e3431ecd44bfdc3] [formerly 7a6712290238d3c0869b21c25ad0317736280043] [formerly 1cf26513b09dfbd440a0033224c9687210a80eb8 [formerly 1cf26513b09dfbd440a0033224c9687210a80eb8 [formerly 658b092879792817765a38a7aeefb55706b4714d]]]]
    Former-commit-id: 56c765d5bdff7877111da07dcc7c84351a9bae77
    Former-commit-id: b5f8123a49d7f2bba617ac0c40165b26cf00f7ed
    Former-commit-id: dfec38bbcf12d01b1eac98c1ab63bccd97d110c7
    Former-commit-id: 39d38a32f41163345a591281c3b88202323f0b72

--- tests/testthat/test_coordinatelevel_functions.R
+++ tests/testthat/test_coordinatelevel_functions.R
@@ -4,16 +4,30 @@ context("Coordinate cleaning")
 set.seed(1)
 exmpl <- data.frame(species = sample(letters, size = 250, replace = TRUE),
                     decimallongitude = runif(250, min = 42, max = 51),
-                    decimallatitude = runif(250, min = -26, max = -11))
+                    decimallatitude = runif(250, min = -26, max = -11),
+                    countrycode = "RUS")
 
 test <- cc_cap(x = exmpl, value = "flags")
 
 summary(test)
+sum(cc_cen(exmpl, value = "flags"))
 
-
-test_that("cc_cap arguments work", {
+test_that("cc_cap works", {
   expect_equal(sum(cc_cap(x = exmpl, value = "flags")), 249)
   expect_equal(sum(cc_cap(x = exmpl, buffer = 250, value = "flags")), 0)
   
   expect_error(cc_cap(x = exmpl, lon = "longitude", value = "flags"), "undefined columns selected")
+})
+
+test_that("cc_cen works", {
+  expect_equal(sum(cc_cen(x = exmpl, value = "flags")), 249)
+  expect_equal(sum(cc_cen(x = exmpl, buffer = 250, value = "flags")), 0)
+  
+  expect_error(cc_cen(x = exmpl, lon = "longitude", value = "flags"), "undefined columns selected")
+})
+
+test_that("cc_coun works", {
+  expect_equal(sum(cc_coun(x = exmpl, value = "flags")), 165)
+  
+  expect_error(cc_coun(x = exmpl, lon = "longitude", value = "flags"), "undefined columns selected")
 })
