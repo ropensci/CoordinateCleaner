2b6b41070e76db9953b416bc2cefb022b58a99a8
azizka <alexander.zizka@bioenv.gu.se>
2018-05-15 14:03:29 +0200

    increased test suite and minor corrections in the documentation
    Former-commit-id: 98870b9ea5b1c5d982aaaecfa281601245e51314 [formerly 98870b9ea5b1c5d982aaaecfa281601245e51314 [formerly 42e4e1b245370aa95260910316db8a9c9e758d36]]
    Former-commit-id: 86542058e081550dac3d22642109d116a1fa8682
    Former-commit-id: cb9ac565773d4ad6828c5f0fae2949b13ad3e2d5

---
+++ tests/testthat/Test_fossillevel_cleaning.R
@@ -0,0 +1,51 @@
+library(CoordinateCleaner)
+context("Fossil cleaning")
+
+set.seed(1)
+#tc_range
+minages <- runif(n = 11, min = 0.1, max = 25)
+test <- data.frame(species = c(letters[1:10], "z"),
+                lng = c(runif(n = 9, min = 4, max = 16), 75, 7),
+                lat = c(runif(n = 11, min = -5, max = 5)),
+                min_ma = minages, 
+                max_ma = minages + c(runif(n = 10, min = 0, max = 5), 25))
+
+
+
+
+test_that("tc_range identifies existing bias", {
+  
+  #return value
+  expect_is(tc_range(test, value = "flags", taxon = ""), "logical")
+  expect_is(tc_range(test, value = "clean", taxon = ""), "data.frame")
+  
+  #outlier method
+  expect_equal(sum(tc_range(test, value = "flags", method = "quantile", taxon = "")), 10)
+  expect_equal(sum(tc_range(test, value = "flags", method = "mad", taxon = "")), 10)
+  expect_equal(sum(tc_range(test, value = "flags", method = "time", taxon = "", max.range = 20, uniq.loc = F)), 10)
+  
+})
+
+
+#tc_outl
+set.seed(1)
+minages <- c(runif(n = 11, min = 10, max = 25), 62.5)
+test <- data.frame(species = c(letters[1:10], rep("z", 2)),
+                lng = c(runif(n = 10, min = 4, max = 16), 75, 7),
+                lat = c(runif(n = 12, min = -5, max = 5)),
+                min_ma = minages, 
+                max_ma = c(minages[1:11] + runif(n = 11, min = 0, max = 5), 65))
+
+
+test_that("tc_outl identifies existing bias", {
+  #return value
+  expect_is(tc_outl(test, value = "flags", taxon = ""), "logical")
+  expect_is(tc_outl(test, value = "clean", taxon = ""), "data.frame")
+  
+  #outlier method
+  expect_equal(sum(tc_outl(test, value = "flags", method = "quantile", taxon = "")), 10)
+  expect_equal(sum(tc_outl(test, value = "flags", method = "quantile", taxon = "", uniq.loc = F)), 10)
+  expect_equal(sum(tc_outl(test, value = "flags", method = "mad", taxon = "")), 10)
+  expect_equal(sum(tc_outl(test, value = "flags", method = "mad", taxon = "", uniq.loc = F)), 10)
+  
+})
