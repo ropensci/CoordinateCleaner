8960dcf5c2b0936b9c6c2242cd5e0329c464a0bc
plantarum <tyler@plantarum.ca>
2025-05-26 12:25:26 -0400

    Switched terra:extract to terra:relate in cc_sea
    In my tests this decreased the processing time for 10K records from 94
    seconds to < 1 second.

--- R/cc_sea.R
+++ R/cc_sea.R
@@ -137,7 +137,8 @@ cc_sea <- function(x,
     ## have to make sure they are the same and in this case they already are
     ## -----
     ## point-in-polygon test
-    ext_dat <- terra::extract(ref, pts)
+    ##ext_dat <- terra::extract(ref, pts)
+    ext_dat <- relate(pts, ref, "intersects", pairs = TRUE, na.rm = FALSE)
     out <- !is.na(ext_dat[!duplicated(ext_dat[, 1]), 2])
     out <- data.frame(terra::geom(pts)[, c("x", "y"), drop = FALSE], out)
     colnames(out)[1:2] <- c(lon, lat)
@@ -154,7 +155,8 @@ cc_sea <- function(x,
     
     # select relevant columns
     #point in polygon test
-    ext_dat <- terra::extract(ref, pts)
+    ext_dat <- relate(pts, ref, "intersects", pairs = TRUE, na.rm = FALSE)
+    ##ext_dat <- terra::extract(ref, pts)
     out <- !is.na(ext_dat[!duplicated(ext_dat[, 1]), 2])
   }
   
